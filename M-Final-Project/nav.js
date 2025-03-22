9b9e58b2d0a6f9f60c71bda9d6e73802f682ffe6
Md.Habibulla <mdhabibulla146425@gmail.com>
2025-03-23 01:13:57 +0600

    some work

--- M-Final-Project/nav.js
+++ M-Final-Project/nav.js
@@ -1,16 +1,16 @@
 const menubar =document.getElementById('menubar');
 const navMid =document.getElementById('navList')
 
-navMid.style.left= '-500px'
+navMid.style.left= '-800px'
 menubar.addEventListener('click',showandHidden)
 
 function showandHidden() {
-    if(navMid.style.left == '-500px'){
+    if(navMid.style.left == '-800px'){
      
         
         navMid.style.left = '0px'
     }else{
-        navMid.style.left = '-500px'
+        navMid.style.left = '-800px'
         
         
     }
