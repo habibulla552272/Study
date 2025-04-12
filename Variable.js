a2bfa345ac7366b79c298f80e5f89d4e70d51179
Md.Habibulla <mdhabibulla146425@gmail.com>
2025-04-12 23:54:12 +0600

    operators

--- Variable.js
+++ Variable.js
@@ -61,6 +61,82 @@ console.log(obj2);
 console.log('good bye bondhura ...');
 
 
+// arithmetic operators -> +,-,*,/,**,%
+
+//Type Casting
+//Type coersion
+
+console.log(10 +  '5'); 
+// answer = 105 
+// + = Casting  
+// because Type coercion 
+// Number + Number = Addition
+// String + Anything = String Concatenation
+
+console.log(10 * '5');
+// asnwer = 50 
+
+console.log(10 - '5');
+// answer = 5
+
+console.log(10 / '5');
+//answer = 2
+
+console.log(15 % '2');
+//answer 1
+
+//Exponentiation **
+console.log(2**4);
+//answer 16
+
+//Increment = ++ 
+let a=1;
+a++
+console.log(a); //answer =2
+
+console.log(a+=1); //answer = 3
+
+// Decrement = --
+let b=5;
+b--;
+console.log(b); //asnwer = 4 
+
+console.log(b-=1); //asnwer = 3
+
+
+//Comparison Operators
+
+//Equal 
+let c=5;
+console.log(c== '5'); //answer true 
+console.log(c=== '5'); //answer false because c=5 this is a number but '5' = string that's why this is a not ture
+
+//Not equal !=,!==
+
+console.log(5 != '5'); //answer false because 5 =number ans and '5' = string but value equal that's why false 
+console.log(5 !== '5'); //answer true because 5 =number ans and '5' = string but value equal but's type not same that's why it's true
+
+//Less then or Greater than 
+console.log(5 >5); //answer = wrong greater than
+console.log( 5>=5); //Answer = true  greater than or equal
+
+console.log(5<3); //answer - false less than because 5 not less than 5 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
