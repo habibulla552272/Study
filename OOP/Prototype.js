c7aa191eb82e0eda157a2d2594278fe708b9f336
Md.Habibulla <mdhabibulla146425@gmail.com>
2025-05-30 19:11:37 +0600

    some add

--- OOP/Prototype.js
+++ OOP/Prototype.js
@@ -1,67 +1,67 @@
-String.prototype.reverse = function () {
-    return this.split("").reverse().join("");
-}
+// String.prototype.reverse = function () {
+//     return this.split("").reverse().join("");
+// }
 
-const name = "Habibulla Islam Habib";
-console.log(name.reverse());
-const courseName = 'MERN Batch 04';
-console.log(courseName.reverse());
+// const name = "Habibulla Islam Habib";
+// console.log(name.reverse());
+// const courseName = 'MERN Batch 04';
+// console.log(courseName.reverse());
 
-Array.prototype.sum = function () {
-    return this.reduce((acc, curr) => acc + curr, 0)
-}
+// Array.prototype.sum = function () {
+//     return this.reduce((acc, curr) => acc + curr, 0)
+// }
 
-const numbers = [1, 3, 5, 7];
-console.log(numbers.sum());
+// const numbers = [1, 3, 5, 7];
+// console.log(numbers.sum());
 
-Array.prototype.even = function () {
-    return this.filter(num => num % 2 === 0);
-}
-const number2 = [1, 3, 6, 7, 33, 10, 55];
-console.log(number2.even());
+// Array.prototype.even = function () {
+//     return this.filter(num => num % 2 === 0);
+// }
+// const number2 = [1, 3, 6, 7, 33, 10, 55];
+// console.log(number2.even());
 
 
-//some add 
+// //some add 
 
-function createUser(name, scroe) {
-    this.name = name
-    this.scroe = scroe
+// function createUser(name, scroe) {
+//     this.name = name
+//     this.scroe = scroe
 
-}
-createUser.prototype.increment = function () {
-    this.scroe++
-}
-createUser.prototype.price = function (price) {
-    this.price = price
-}
-createUser.prototype.printMe = function () {
-    console.log(`score is ${this.scroe} `);
+// }
+// createUser.prototype.increment = function () {
+//     this.scroe++
+// }
+// createUser.prototype.price = function (price) {
+//     this.price = price
+// }
+// createUser.prototype.printMe = function () {
+//     console.log(`score is ${this.scroe} `);
 
-}
-createUser.prototype.printPrice = function () {
+// }
+// createUser.prototype.printPrice = function () {
 
-    console.log(`${this.name} price is ${this.price}`);
+//     console.log(`${this.name} price is ${this.price}`);
 
-}
+// }
 
-const chai = new createUser('chai', 25);
-const tea = new createUser('tea', 15);
+// const chai = new createUser('chai', 25);
+// const tea = new createUser('tea', 15);
 
-chai.printMe();
-chai.increment();
-chai.price(50)
-chai.printMe();
-tea.printMe();
-tea.price(100)
-tea.printPrice()
-chai.printPrice()
+// chai.printMe();
+// chai.increment();
+// chai.price(50)
+// chai.printMe();
+// tea.printMe();
+// tea.price(100)
+// tea.printPrice()
+// chai.printPrice()
 
 
 //prototype example
 
-let myname = 'habibulla     ';
-console.log(myname.length);
-console.log(myname.trim().length);
+// let myname = 'habibulla     ';
+// console.log(myname.length);
+// console.log(myname.trim().length);
 
 let myheros = ['thor', 'iron man'];
 
@@ -77,3 +77,10 @@ let heroPower = {
 }
 
 
+Object.prototype.habibula=function(){
+    console.log(`habibulla is present in all of  object`);
+    
+}
+
+heroPower.habibula();
+myheros.habibula();
--- OOP/inheritance.js
+++ OOP/inheritance.js
@@ -1,3 +1,37 @@
+
+//old object inheritance concept 
+
+const user={
+    name:'habibulla',
+    gmail:'@habibullaislam'
+}
+const teacher={
+    makeVideo:true,
+
+}
+const teachingSupport={
+    inAvailable:false,
+    name:'Habu'
+}
+const TASupport={
+    makeAssignment:'js assignment',
+    fullTime:true,
+    __proto__:teachingSupport
+}
+
+console.log(TASupport.name);
+
+teacher.__proto__=user;
+console.log(teacher.name);
+console.log(teacher.gmail);
+
+//morder syntax inheritance 
+Object.setPrototypeOf(teacher,TASupport);
+console.log(teacher.fullTime);
+
+
+
+// new concept in class inheritance
 class Animal{
     constructor(name){
         this.name=name;
