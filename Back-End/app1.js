2011e63accc0eba9e345bb0a7e7a3667e461cf8e
Md.Habibulla <mdhabibulla146425@gmail.com>
2025-09-15 18:08:54 +0600

    app1 update

--- Back-End/app1.js
+++ Back-End/app1.js
@@ -1,13 +1,25 @@
-const http= require('http');
-const PORT= 8080;
+const http = require('http');
+const PORT = 8080;
 
-const server=http.createServer((req,res)=>{
-    res.writeHead(200,{'content-type':'text/olaintext'});
-    res.end("hello world");
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        if (req.method == 'GET') {
+            res.writeHead(200, { 'content-type': 'text/olaintext' });
+            res.end("hello page");
+        }
+    } else if (req.url === '/api/users') {
+        if (req.method === "GET") {
+            res.writeHead(200, { 'content-type': 'application/json' });
+            res.end("hello users");
+        }
+    } else {
+        res.writeHead(404);
+        res.end("Not Found");
+    }
 })
 
 
-server.listen(PORT,()=>{
+server.listen(PORT, () => {
     console.log(`server is runing http://localhost:${PORT}`);
-    
+
 })
