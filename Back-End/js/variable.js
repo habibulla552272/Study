8a42827ea621eb1b62d2089d078dedc2f2298621
Md.Habibulla <mdhabibulla146425@gmail.com>
2025-09-22 06:57:45 +0600

    let const defarent

---
+++ Back-End/js/variable.js
@@ -0,0 +1,20 @@
+// let 
+let h = 'habibulla';
+console.log(h);
+h='islam';
+console.log(h) //it's okay it can be do that 
+// but it can not be do that 
+// let h= 'ali';  i need to commet this line not error showing 
+// console.log(h) // show this message SyntaxError: Identifier 'h' has already been declared
+
+
+// learn  const 
+// const can't not redecliar and reassign  but we can try it 
+const i= 'iftrkhar';
+// const i= 'iftekhar alam'; // it can be possibal show this message SyntaxError: Identifier 'i' has already been declared
+// console.log(i);
+
+// and also it's can not be possibal to reassignable 
+// but we can  try then see what happen to us 
+// i='iftekhar alam'; // see show this message TypeError: Assignment to constant variable.
+// console.log(i);
