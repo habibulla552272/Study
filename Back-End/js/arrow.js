e14c6fa200ac615eb4ebdeb4c29079c944f207d1
Md.Habibulla <mdhabibulla146425@gmail.com>
2025-09-22 10:27:23 +0600

    arrow funtion ad

---
+++ Back-End/js/arrow.js
@@ -0,0 +1,32 @@
+// right now i learn arrow function 
+function great(){
+    return 'hello';
+}
+const great1=()=> 'hello'
+console.log(great())
+console.log(great1())
+
+// 3. lexical this binding 
+
+// let's co try it 
+const person={
+    name:'habibulla',
+    great:function(){
+        setTimeout(()=>{
+            console.log('hello ,'+ this.name)
+        },1000)
+    }
+}
+person.great();
+
+// normal function
+
+const person2={
+    name:'habibulla',
+    great:function(){
+        setTimeout(function(){
+            console.log('hello,'+this.name)
+        },100)
+    }
+}
+person2.great()
--- Back-End/js/variable.js
+++ Back-End/js/variable.js
@@ -2,19 +2,19 @@
 let h = 'habibulla';
 console.log(h);
 h='islam';
-console.log(h) //it's okay it can be do that 
-// but it can not be do that 
+console.log(h) //it's okay . we  can  do that 
+// but it is not allow  
 // let h= 'ali';  i need to commet this line not error showing 
 // console.log(h) // show this message SyntaxError: Identifier 'h' has already been declared
 
 
 // learn  const 
-// const can't not redecliar and reassign  but we can try it 
+// const can not be redeclared and reassigned  but we can try it.
 const i= 'iftrkhar';
 // const i= 'iftekhar alam'; // it can be possibal show this message SyntaxError: Identifier 'i' has already been declared
 // console.log(i);
 
-// and also it's can not be possibal to reassignable 
-// but we can  try then see what happen to us 
+// and also it is  not  possibal to reassign 
+// but we can  try and see what happens
 // i='iftekhar alam'; // see show this message TypeError: Assignment to constant variable.
 // console.log(i);
