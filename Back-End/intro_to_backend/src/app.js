abcd793acd285a6e12c73ecacfd2972401708d30
Md.Habibulla <mdhabibulla146425@gmail.com>
2026-04-30 20:09:29 +0600

    some code added

--- Back-End/intro_to_backend/src/app.js
+++ Back-End/intro_to_backend/src/app.js
@@ -6,6 +6,13 @@ const app = express();
 app.use(express.json());
 // routes import
 
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+    uptime: process.uptime(),
+  });
+});
+
 // router declaration
 
 app.use("/api/v1/users", userRouter);
@@ -13,4 +20,20 @@ app.use("/api/v1/posts", postRouter);
 app.get("/", (req, res) => {
   res.send("Welcome to the API");
 });
+
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+  });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+  });
+});
+
 export default app;
