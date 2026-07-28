ded266b439e3f3398123a7c0a912ae4ee83f0035
MD Habibulla <mdhabibulla146425@gmail.com>
2026-07-29 00:00:34 +0600

    fixed

--- Fron-End/redux/src/app/page.tsx
+++ Fron-End/redux/src/app/page.tsx
@@ -3,9 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      di
-      div
-      
+     <div>
+
+      <h2>Habibulla Islam habib</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit assumenda modi nisi, minima, quo laudantium consequatur dignissimos dolor iure neque aperiam quisquam tempora nihil. Accusantium numquam soluta magnam veniam libero!</p>
+     </div>
+
     </div>
   );
 }
