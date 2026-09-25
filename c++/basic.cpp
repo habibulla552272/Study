58bc8f62dc774d2e4d49f3bb701f4d0f1aa55b33
MD Habibulla <mdhabibulla146425@gmail.com>
2026-09-26 00:26:03 +0600

    input output and data type

--- c++/basic.cpp
+++ c++/basic.cpp
@@ -1,9 +1,28 @@
+// basic structure
 #include <iostream>
 using namespace std;
 
-int main() {
-    int a;
-    cin >> a;
-    cout << a << endl;
+int main(){
+    // why we use uning name std in top 
+//     int i,j,h;
+//    std::cin >> i;//standard namespace  std mean i we use in top then it's okay 
+//    std::cout << "this is the value " << i << std::endl;
+
+// multipul input output
+    //    int i,j,h;
+    //    double d;
+    //    char c;
+
+    //   cin >> i >> j >> h >> d >> c;
+    //   cout << i << " " << j << " " << h  << " "<< d << " " << c << endl;
+        // string name;
+        // // cin >> name;
+        // // cout << name << endl;
+        // cout << "write your full name in one line: ";
+        // getline(cin,name);
+        // cout << name << endl;
+//
+// condition 
+
     return 0;
 }
